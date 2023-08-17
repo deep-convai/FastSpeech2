42607f76f17bedb558f0c193ab109f0566f172b4
DeepankarAcharyya <deepankaracharyya@gmail.com>
2023-08-18 00:09:29 +0530

    updated the wandb logging

--- utils/tools.py
+++ utils/tools.py
@@ -94,21 +94,22 @@ def log(
     log_dict = {}
     
     if losses is not None:
-        log_dict[logger_stage + "Loss/total_loss"] = losses[0]
-        log_dict[logger_stage + "Loss/mel_loss"] = losses[1]
-        log_dict[logger_stage + "Loss/mel_postnet_loss"] = losses[2]
-        log_dict[logger_stage + "Loss/pitch_loss"] = losses[3]
-        log_dict[logger_stage + "Loss/energy_loss"] = losses[4]
-        log_dict[logger_stage + "Loss/duration_loss"] = losses[5]
+        log_dict[logger_stage + "_Loss/total_loss"] = losses[0]
+        log_dict[logger_stage + "_Loss/mel_loss"] = losses[1]
+        log_dict[logger_stage + "_Loss/mel_postnet_loss"] = losses[2]
+        log_dict[logger_stage + "_Loss/pitch_loss"] = losses[3]
+        log_dict[logger_stage + "_Loss/energy_loss"] = losses[4]
+        log_dict[logger_stage + "_Loss/duration_loss"] = losses[5]
+        wandb.log(log_dict, step=step)
         
-    if fig is not None:
-        log_dict[logger_stage + tag] = wandb.Image(fig)
+    # if fig is not None: ----------- we dont need the figure logging for now
+        # log_dict[logger_stage + tag] = wandb.Image(fig)
 
-    if audio is not None:
-        norm_audio = audio / max(abs(audio))
-        log_dict[logger_stage + tag] = wandb.Audio(norm_audio, sample_rate=sampling_rate)
+    # if audio is not None: ---------- we dont need the audio logging for now
+    #     norm_audio = audio / max(abs(audio))
+    #     log_dict[logger_stage + tag] = wandb.Audio(norm_audio, sample_rate=sampling_rate)
     
-    wandb.log(log_dict, step=step)
+    
 
 def get_mask_from_lengths(lengths, max_len=None):
     batch_size = lengths.shape[0]
